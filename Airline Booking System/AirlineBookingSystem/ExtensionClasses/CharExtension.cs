499858443987bb620bbccf990364175f057e8ceb
kriyandi <kriyandimitrov@gmail.com>
2021-04-19 15:26:20 +0300

    Add CharExtension class

---
+++ Airline Booking System/AirlineBookingSystem/ExtensionClasses/CharExtension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirlineBookingSystem.ExtensionClasses
+{
+    public static class CharExtension
+    {
+        public static bool IsLetterBetweenAAndJ(this char str)
+        {
+            return ('A' <= str) && (str <= 'J');
+        }
+    }
+}
