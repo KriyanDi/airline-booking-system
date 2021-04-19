d8e9d02a54c154afb0cb1ede84bfb9c05abe936d
kriyandi <kriyandimitrov@gmail.com>
2021-04-19 13:25:51 +0300

    Add StringExtension class
    The code in Airline,Airport classes started to repeat for specific functions so StringExtension class was made to wrap those functions.

---
+++ Airline Booking System/AirlineBookingSystem/StringExtension.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AirlineBookingSystem
+{
+    public static class StringExtension
+    {
+        public static bool HasOnlyCapitalLetters(this String str)
+        {
+            return Regex.IsMatch(str, @"^[A-Z]+$");
+        }
+
+        public static bool HasLengthThree(this String str)
+        {
+            return str.Length == 3;
+        }
+
+        public static bool HasLengthLessThanSix(this String str)
+        {
+            return str.Length < 6;
+        }
+    }
+}
