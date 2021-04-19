866f9dfaa28bd2033dbc05f9007d8adde2cfee9f
kriyandi <kriyandimitrov@gmail.com>
2021-04-19 16:12:38 +0300

    Change SeatClass namespace

--- Airline Booking System/AirlineBookingSystem/Seat/SeatClass.cs
+++ Airline Booking System/AirlineBookingSystem/Seat/SeatClass.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AirlineBookingSystem.SeatClass
+namespace AirlineBookingSystem.Seat
 {
     public enum SeatClass
     {
