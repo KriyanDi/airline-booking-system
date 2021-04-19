2e41c133eb74fe58c41f9ef15302f10083d5d52b
kriyandi <kriyandimitrov@gmail.com>
2021-04-19 13:27:04 +0300

    Add Airline class

---
+++ Airline Booking System/AirlineBookingSystem/Airline/Airline.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AirlineBookingSystem.Airline
+{
+    class Airline
+    {
+        private string _name;
+
+        public string Name
+        {
+            get => _name;
+            set 
+            {
+                if (value != null)
+                {
+                    if (value.HasLengthLessThanSix())
+                    {
+                        if (value.HasOnlyCapitalLetters())
+                        {
+                            _name = value;
+                        }
+                        else
+                        {
+                            throw new ArgumentException("Airline name should contain only capital letters.");
+                        }
+                    }
+                    else
+                    {
+                        throw new ArgumentException("Airline name should be less than 6 letters long.");
+                    }
+                }
+                else
+                {
+                    throw new ArgumentNullException("Airline name can not be null.");
+                }
+            }
+        }
+    }
+}
