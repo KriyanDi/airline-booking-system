919aba949481961e3269a7e4320e6ce3978d6015
kriyandi <kriyandimitrov@gmail.com>
2021-04-20 14:43:04 +0300

    Add FlightInformation class

---
+++ Airline Booking System/AirlineBookingSystem/FlightInformation.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace AirlineBookingSystem
+{
+    public class FlightInformation
+    {
+        #region Fields
+        private string _airlineName;
+        private string _originatingAirport;
+        private string _destinationAirport;
+        private int _flightNumber;
+        private DateTime _departureDate;
+        private string _id;
+
+        public FlightInformation(string airlineName, string originatingAirport, string destinationAirport, int flightNumber, DateTime departureDate, string id)
+        {
+            AirlineName = airlineName;
+            OriginatingAirport = originatingAirport;
+            DestinationAirport = destinationAirport;
+            FlightNumber = flightNumber;
+            DepartureDate = departureDate;
+            Id = id;
+        }
+        public FlightInformation(FlightInformation other):this(other.AirlineName,other.OriginatingAirport, other.DestinationAirport,other.FlightNumber, other.DepartureDate, other.Id)
+        {
+        }
+        #endregion
+
+        #region Properties
+        public string AirlineName
+        {
+            get => _airlineName;
+            private set => _airlineName = value;
+        }
+        public string OriginatingAirport
+        {
+            get => _originatingAirport;
+            private set => _originatingAirport = value;
+        }
+        public string DestinationAirport
+        {
+            get => _destinationAirport;
+            private set => _destinationAirport = value;
+        }
+        public int FlightNumber
+        {
+            get => _flightNumber;
+            private set => _flightNumber = value;
+        }
+        public DateTime DepartureDate
+        {
+            get => _departureDate;
+            private set => _departureDate = value;
+        }
+        public string Id
+        {
+            get => _id;
+            private set => _id = value;
+        }
+        #endregion
+    }
+}
