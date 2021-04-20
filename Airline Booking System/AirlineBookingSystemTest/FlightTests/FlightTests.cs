b19946ae468507ffeab3d765f2c5721e91cea454
kriyandi <kriyandimitrov@gmail.com>
2021-04-20 14:43:51 +0300

    Add FlightTests class

---
+++ Airline Booking System/AirlineBookingSystemTest/FlightTests/FlightTests.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using AirlineBookingSystem;
+
+namespace AirlineBookingSystemTest.FlightTests
+{
+    public class FlightTests
+    {
+        private FlightInformation info = new FlightInformation("AAAA", "QWE", "RTY", 123456789, new DateTime(2021, 5, 3), null);
+        private FlightSection section1 = new FlightSection(SeatClass.BUSINESS, 45, 6);
+        private FlightSection section2 = new FlightSection(SeatClass.ECONOMY, 45, 6);
+        private FlightSection section3 = new FlightSection(SeatClass.FIRST, 45, 6);
+
+        [Fact]
+        public void Constructor_MakingObjectNoFlightSections_ShouldPassTest()
+        {
+            // Arrange 
+            FlightInformation expected = new FlightInformation(info);
+
+            // Act
+            Flight actual = new Flight(info.AirlineName, info.OriginatingAirport, info.DestinationAirport, info.FlightNumber, info.DepartureDate);
+
+            // Assert
+            Assert.Equal(expected.AirlineName, actual.Information.AirlineName);
+            Assert.Equal(expected.OriginatingAirport, actual.Information.OriginatingAirport);
+            Assert.Equal(expected.DestinationAirport, actual.Information.DestinationAirport);
+            Assert.Equal(expected.DepartureDate, actual.Information.DepartureDate);
+            Assert.Equal(expected.Id, actual.Information.Id);
+            Assert.Null(actual.FlightSections);
+        }
+
+        [Fact]
+        public void Constuctor_MakingObjectWithTheSameAirportForOrginiateAndDestination_ShouldThrowException()
+        {
+            // Arrange
+            ArgumentException expected = new ArgumentException("Originating Airport and Destination Airport can not be the same.");
+
+            // Act
+            ArgumentException actual = null;
+            try
+            {
+                Flight flight = new Flight(info.AirlineName, info.OriginatingAirport, info.OriginatingAirport, info.FlightNumber, info.DepartureDate);
+            }
+            catch (ArgumentException ex)
+            {
+                actual = ex;
+            }
+
+            // Assert
+            Assert.Equal(expected.Message, actual.Message);
+        }
+
+        [Fact]
+        public void CopyConstructor_MakingObject_ShouldPass()
+        {
+            // Arrange
+            Flight flight = new Flight(info.AirlineName, info.OriginatingAirport, info.DestinationAirport, info.FlightNumber, info.DepartureDate);
+            flight.AddFlightSection(section1);
+            flight.AddFlightSection(section2);
+            flight.AddFlightSection(section3);
+
+            // Act
+            Flight flightCopy = new Flight(flight);
+
+            bool HasSameSequentialElements = true;
+            for (int i = 0; i < flightCopy.FlightSections.Count; i++)
+            {
+                if(flight.FlightSections[i].Equals(flightCopy.FlightSections[i]))
+                {
+                    HasSameSequentialElements = false;
+                    break;
+                }
+            }
+
+            // Assert
+            Assert.Equal(flight.Information.AirlineName, flightCopy.Information.AirlineName);
+            Assert.Equal(flight.Information.OriginatingAirport, flightCopy.Information.OriginatingAirport);
+            Assert.Equal(flight.Information.DestinationAirport, flightCopy.Information.DestinationAirport);
+            Assert.Equal(flight.Information.DepartureDate, flightCopy.Information.DepartureDate);
+            Assert.Equal(flight.Information.Id, flightCopy.Information.Id);
+            Assert.Equal(flight.FlightSections.Count, flightCopy.FlightSections.Count);
+           
+            Assert.Equal(flight.FlightSections, flightCopy.FlightSections);
+            
+            Assert.True(HasSameSequentialElements);
+        }
+
+        [Fact]
+        public void AddFlightSection_AddingTwoDifferentFlightSections_ShouldPassTest()
+        {
+            // Arrange 
+
+            // Act
+            Flight actual = new Flight(info.AirlineName, info.OriginatingAirport, info.DestinationAirport, info.FlightNumber, info.DepartureDate);
+            actual.AddFlightSection(section1);
+            actual.AddFlightSection(section2);
+
+            // Assert
+            Assert.NotNull(actual.FlightSections);
+            Assert.Equal(2, actual.FlightSections.Count);
+        }
+
+        [Fact]
+        public void AddFlightSection_AddingTwoExactFlightSections_ShouldThrowExceptionTest()
+        {
+            // Arrange 
+            ArgumentException expected = new ArgumentException("This flight section already exist! Can store only one of each kind of sections.");
+
+            // Act
+            ArgumentException actual = null;
+            Flight flight = new Flight(info.AirlineName, info.OriginatingAirport, info.DestinationAirport, info.FlightNumber, info.DepartureDate);
+            flight.AddFlightSection(section1);
+
+            try
+            {
+                flight.AddFlightSection(section1);
+            }
+            catch (ArgumentException ex)
+            {
+                actual = ex;
+            }
+
+            // Assert
+            Assert.Equal(expected.Message, actual.Message);
+        }
+    }
+}
