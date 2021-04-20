29bc03d83dd5476b78a5f4568152dac704056ed8
kriyandi <kriyandimitrov@gmail.com>
2021-04-20 15:40:22 +0300

    Modify Airline class
    Now class can holds flights

--- Airline Booking System/AirlineBookingSystem/Airline.cs
+++ Airline Booking System/AirlineBookingSystem/Airline.cs
@@ -11,14 +11,19 @@ namespace AirlineBookingSystem
     {
         #region Fields
         private string _name;
+        private List<Flight> _flights;
+        private static int flightsCounter;
         #endregion
 
         #region Constructors
+        static Airline()
+        {
+            flightsCounter = 0;
+        }
         public Airline(string name)
         {
             Name = name;
         }
-
         public Airline(Airline other) : this(other.Name)
         {
         }
@@ -53,6 +58,59 @@ namespace AirlineBookingSystem
                     throw new ArgumentNullException("Airline name can not be null.");
                 }
             }
+        }
+        public List<Flight> Flights
+        {
+            get
+            {
+                List<Flight> flightsCopy = null;
+
+                if (_flights != null)
+                {
+                    flightsCopy = new List<Flight>();
+                    for (int i = 0; i < _flights.Count; i++)
+                    {
+                        flightsCopy.Add(new Flight(_flights[i]));
+                    }
+                }
+
+                return flightsCopy;
+            }
+
+            set
+            {
+                List<Flight> flightsCopy = null;
+
+                if (value != null)
+                {
+                    flightsCopy = new List<Flight>();
+                    for (int i = 0; i < value.Count; i++)
+                    {
+                        flightsCopy.Add(new Flight(value[i]));
+                    }
+                }
+
+                _flights = flightsCopy;
+            }
+        }
+        #endregion
+
+        #region Methods
+        public bool AddFlight(Flight flight)
+        {
+            flightsCounter += 1;
+
+            Flight flightCopy = new Flight(flight);
+            flightCopy.ChangeFlightInformationId($"{Name}{flightsCounter:D5}");
+
+            if (_flights == null)
+            {
+                _flights = new List<Flight>();
+            }
+
+            _flights.Add(new Flight(flightCopy));
+            Console.WriteLine($"{Name}");
+            return true;
         }
         #endregion
     }
