047274af05f957344a546f40db7cc9109ef5f53c
kriyandi <kriyandimitrov@gmail.com>
2021-04-21 09:28:30 +0300

    Add SystemManagerTests class with tests for CreateAirport method

---
+++ Airline Booking System/AirlineBookingSystemTest/SystemManagerTests/SystemManagerTests.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AirlineBookingSystem;
+using Xunit;
+
+namespace AirlineBookingSystemTest.SystemManagerTests
+{
+    public class SystemManagerTests
+    {
+        #region CreateAirportMethodTests
+        [Fact]
+        public void CreateAirport_WhenObjectIsCreatedItContainsZeroAirports_ShouldPassTest()
+        {
+            // Arrange
+            SystemManager manager = new SystemManager();
+            int expected = 0;
+
+            // Act
+            int actual = manager.Airports.Count;
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void CreateAirport_AddingValidNewAirportToEmptyAirportList_ShouldPassTest()
+        {
+            // Arrange
+            SystemManager manager = new SystemManager();
+            Airport expected = new Airport("QWE");
+
+            // Act
+            manager.CreateAirport("QWE");
+            Airport actual = manager.Airports[0];
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void CreateAirport_AddingValidAirportToAlreadyContainingSomeAirportsAirportsList_ShouldPassTest()
+        {
+            // Arrange
+            SystemManager manager = new SystemManager();
+            manager.CreateAirport("QWE");
+            manager.CreateAirport("ASD");
+            manager.CreateAirport("YUI");
+            Airport expected = new Airport("SSS");
+
+            // Act
+            manager.CreateAirport("SSS");
+            Airport actual = manager.Airports[3];
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void CreateAirport_AddingInvalidNewAirportThatHasLongerThanThreeLettersName_ShoudlThrowExceptionTest()
+        {
+            // Arrange
+            SystemManager manager = new SystemManager();
+            ArgumentException expected = new ArgumentException("Airport name should be 3 letters long.");
+
+            // Act
+            ArgumentException actual = null;
+            try
+            {
+                manager.CreateAirport("ASD23");
+            }
+            catch (ArgumentException ex)
+            {
+                actual = ex;
+            }
+
+            // Assert
+            Assert.Equal(expected.Message, actual.Message);
+        }
+
+        [Fact]
+        public void CreateAirport_AddingInvalidNewAirportThatHasShorterThanThreeLettersName_ShoudlThrowExceptionTest()
+        {
+            // Arrange
+            SystemManager manager = new SystemManager();
+            ArgumentException expected = new ArgumentException("Airport name should be 3 letters long.");
+
+            // Act
+            ArgumentException actual = null;
+            try
+            {
+                manager.CreateAirport("AS");
+            }
+            catch (ArgumentException ex)
+            {
+                actual = ex;
+            }
+
+            // Assert
+            Assert.Equal(expected.Message, actual.Message);
+        }
+
+        [Fact]
+        public void CreateAirport_AddingInvalidNewAirportThatIsEmptyStringName_ShoudlThrowExceptionTest()
+        {
+            // Arrange
+            SystemManager manager = new SystemManager();
+            ArgumentException expected = new ArgumentException("Airport name should be 3 letters long.");
+
+            // Act
+            ArgumentException actual = null;
+            try
+            {
+                manager.CreateAirport("");
+            }
+            catch (ArgumentException ex)
+            {
+                actual = ex;
+            }
+
+            // Assert
+            Assert.Equal(expected.Message, actual.Message);
+        }
+
+        [Fact]
+        public void CreateAirport_AddingInvalidNewAirportThatIsNullString_ShoudlThrowExceptionTest()
+        {
+            // Arrange
+            SystemManager manager = new SystemManager();
+            ArgumentNullException expected = new ArgumentNullException("Airport name can not be null.");
+            string airportName = null;
+            // Act
+            ArgumentNullException actual = null;
+            try
+            {
+                manager.CreateAirport(airportName);
+            }
+            catch (ArgumentNullException ex)
+            {
+                actual = ex;
+            }
+
+            // Assert
+            Assert.Equal(expected.Message, actual.Message);
+        }
+
+        [Fact]
+        public void CreateAirport_AddingInvalidNewAirportThatHasThreeSymbolsButNotOnlyLettersName_ShoudlThrowExceptionTest()
+        {
+            // Arrange
+            SystemManager manager = new SystemManager();
+            ArgumentException expected = new ArgumentException("Airport name should contain only capital letters.");
+
+            // Act
+            ArgumentException actual = null;
+            try
+            {
+                manager.CreateAirport("e@2");
+            }
+            catch (ArgumentException ex)
+            {
+                actual = ex;
+            }
+
+            // Assert
+            Assert.Equal(expected.Message, actual.Message);
+        }
+
+        [Fact]
+        public void CreateAirport_AddingAlreadyExistingAirport_ShouldThrowExceptionTest()
+        {
+            // Arrange
+            SystemManager manager = new SystemManager();
+            manager.CreateAirport("QWE");
+            manager.CreateAirport("ASD");
+            manager.CreateAirport("YUI");
+            ArgumentException expected = new ArgumentException("Airport YUI already exists!");
+
+            // Act
+            ArgumentException actual = null;
+            try
+            {
+                manager.CreateAirport("YUI");
+            }
+            catch (ArgumentException ex)
+            {
+                actual = ex;
+            }
+
+            // Assert
+            Assert.Equal(actual.Message, actual.Message);
+        } 
+        #endregion
+
+
+    }
+}
