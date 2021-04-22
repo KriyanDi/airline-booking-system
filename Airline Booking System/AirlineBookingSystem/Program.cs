9bed5d9b144e0d0b3e9c7b0cd4caa8f831782a5d
kriyandi <kriyandimitrov@gmail.com>
2021-04-22 11:28:25 +0300

    Modify selected files

--- Airline Booking System/AirlineBookingSystem/Program.cs
+++ Airline Booking System/AirlineBookingSystem/Program.cs
@@ -10,6 +10,21 @@ namespace AirlineBookingSystem
     {
         static void Main(string[] args)
         {
+            // Arrange
+            ArgumentException expected = new ArgumentException("Could not book seat on Row: 5 Col: A!");
+            SystemManager system = new SystemManager();
+
+            system.CreateAirport("AAA");
+            system.CreateAirport("BBB");
+
+            system.CreateAirline("ASD");
+
+            system.CreateFlight("ASD", "AAA", "BBB", 1992, 4, 5, "1543");
+
+            system.CreateSection("ASD", "1543", 5, 4, SeatClass.BUSINESS);
+
+            system.BookSeat("ASD", "1543", SeatClass.BUSINESS, 5, 'A');
+            system.BookSeat("ASD", "1543", SeatClass.BUSINESS, 5, 'A');
         }
     }
 }
