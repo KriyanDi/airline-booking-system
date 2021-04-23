bf189f12ce071f6b9c576b755eebea8ee25f8b2f
kriyandi <kriyandimitrov@gmail.com>
2021-04-23 23:59:39 +0300

    Add ExceptionMessages class

---
+++ Airline Booking System/AirlineBookingSystem/ExceptionMessages.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirlineBookingSystem
+{
+    public class AirportExceptionMessages
+    {
+        public static string invalidName = "Invalid Airport Name!";
+    }
+
+    public class AirlineExceptionMessages
+    {
+        public static string invalidName = "Invalid Airline Name!";
+    }
+
+    public class FlightExceptionMessages
+    {
+        public static string invalidMatchingOriginatingDestinationAirports = "Matching Originating And Destination Airport!";
+    }
+
+    public class SystemManagerExceptionMessages
+    {
+        public static string invalidFlightSectionSeatClass = "Flight does not contain section with such seat class";
+    }
+}
