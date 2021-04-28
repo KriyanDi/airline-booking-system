ef7661cb76c3bd6dc640ddb5eaf34e1e77975078
kriyandi <kriyandimitrov@gmail.com>
2021-04-28 17:21:00 +0300

    Modify FlightSection class

--- Airline Booking System/AirlineBookingSystem/Flights/FlightSection.cs
+++ Airline Booking System/AirlineBookingSystem/Flights/FlightSection.cs
@@ -8,101 +8,36 @@ namespace AirlineBookingSystem
 {
     public class FlightSection
     {
-        #region Fields
-        private SeatClass _seatClass;
-        private List<Seat> _seats;
-        #endregion
-
         #region Constructors
         public FlightSection(SeatClass seatClass, int rows, int cols)
         {
-            InitializeDataMembers(seatClass, rows, cols);
+            SeatClass = seatClass;
+            InitializeSeats(rows, cols);
         }
         public FlightSection(FlightSection other)
         {
-            InitializeDataMembersFrom(other);
+            SeatClass = other.SeatClass;
+            InitializeSeatsFrom(other.Seats);
         }
         #endregion
 
         #region Properties
-        public SeatClass SeatClass
-        {
-            get => _seatClass;
-            set => _seatClass = value;
-        }
-        public List<Seat> Seats
-        {
-            get
-            {
-                List<Seat> seatsCopy = new List<Seat>();
-                for (int i = 0; i < _seats.Count; i++)
-                {
-                    seatsCopy.Add(new Seat(_seats[i]));
-                }
-
-                return seatsCopy;
-            }
-            set
-            {
-                if (value != null)
-                {
-                    List<Seat> seatsCopy = new List<Seat>();
-                    for (int i = 0; i < value.Count; i++)
-                    {
-                        seatsCopy.Add(new Seat(_seats[i]));
-                    }
-
-                    _seats = seatsCopy;
-                }
-                else
-                {
-                    throw new ArgumentNullException("Value can not be null.");
-                }
-            }
-        }
-        public List<Seat> SeatReference => _seats;
-        public int CountAvailableSeats
-        {
-            get
-            {
-                int available = 0;
-                for (int i = 0; i < _seats.Count; i++)
-                {
-                    if(!_seats[i].IsBooked)
-                    {
-                        available += 1;
-                    }
-                }
-
-                return available;
-            }
-        }
-        public int CountOccupiedSeats => _seats.Count - CountAvailableSeats;
+        public SeatClass SeatClass { get; set; }
+        public List<Seat> Seats { get; set; }
+        public int CountAvailableSeats => Seats.Count(seat => !seat.IsBooked);
+        public int CountOccupiedSeats => Seats.Count - CountAvailableSeats;
         #endregion
 
         #region Methods
-        public bool HasAvailableSeats()
-        {
-            bool result = false;
-
-            foreach (Seat seat in _seats)
-            {
-                if (!seat.IsBooked)
-                {
-                    result = true;
-                    break;
-                }
-            }
-
-            return result;
-        }
+        public bool HasAvailableSeats() => Seats.Any(seat => !seat.IsBooked);
         public bool BookSeat()
         {
-            for (int i = 0; i < _seats.Count; i++)
+            for (int i = 0; i < Seats.Count; i++)
             {
-                if (!_seats[i].IsBooked)
+                if (!Seats[i].IsBooked)
                 {
-                    _seats[i].IsBooked = true;
+                    Seats[i].IsBooked = true;
+
                     return true;
                 }
             }
@@ -111,13 +46,13 @@ namespace AirlineBookingSystem
         }
         public bool BookSeat(int rows, char cols)
         {
-            for (int i = 0; i < _seats.Count; i++)
+            for (int i = 0; i < Seats.Count; i++)
             {
-                if (_seats[i].Id == (rows, cols))
+                if (Seats[i].Id == (rows, cols))
                 {
-                    if (!_seats[i].IsBooked)
+                    if (!Seats[i].IsBooked)
                     {
-                        _seats[i].IsBooked = true;
+                        Seats[i].IsBooked = true;
                         return true;
                     }
                     else
@@ -132,85 +67,39 @@ namespace AirlineBookingSystem
         #endregion
 
         #region Help Methods
-        private void InitializeDataMembers(SeatClass seatClass, int rows, int cols)
-        {
-            SeatClass = seatClass;
-            Seats = new List<Seat>();
-            InitializeSeats(rows, cols);
-        }
-        private void InitializeDataMembersFrom(FlightSection other)
-        {
-            SeatClass = other.SeatClass;
-            InitializeSeats(other.Seats);
-        }
         private void InitializeSeats(int rows, int cols)
         {
-            if (AreValidRowCol(rows, cols))
+            Seats = new List<Seat>();
+
+            for (int i = 0; i < rows; i++)
             {
-                for (int i = 0; i < rows; i++)
+                for (int j = 0; j < cols; j++)
                 {
-                    for (int j = 0; j < cols; j++)
-                    {
-                        (int rows, char cols) seatId = (i + 1, Convert.ToChar(65 + (j % cols)));
-                        _seats.Add(new Seat(seatId, false));
-                    }
+                    Seats.Add(new Seat((i + 1, Convert.ToChar(65 + (j % cols))), false));
                 }
             }
-            else
-            {
-                throw new ArgumentException("Row or Col are invalid.");
-            }
         }
-        private void InitializeSeats(List<Seat> seats)
+        private void InitializeSeatsFrom(List<Seat> seats)
         {
-            _seats = new List<Seat>();
+            Seats = new List<Seat>();
+
             for (int i = 0; i < seats.Count; i++)
             {
-                _seats.Add(new Seat(seats[i]));
+                Seats.Add(new Seat(seats[i]));
             }
-        }
-        private static bool AreValidRowCol(int rows, int cols)
-        {
-            bool isRowValid = (1 <= rows && rows <= 100);
-
-            bool isColValid = (1 <= cols && cols <= 10);
-
-            return isRowValid && isColValid;
         }
         #endregion
 
         #region Equation Methods
         public override bool Equals(object obj)
         {
-            if ((obj == null) || !this.GetType().Equals(obj.GetType()))
-            {
-                return false;
-            }
-            else if (obj is FlightSection flightSection)
-            {
-                for (int i = 0; i < Seats.Count; i++)
-                {
-                    if (!(Seats[i].Equals(flightSection.Seats[i])))
-                    {
-                        return false;
-                    }
-                }
-
-                return this.SeatClass == flightSection.SeatClass &&
-                       this.Seats.Count == flightSection.Seats.Count &&
-                       Seats.SequenceEqual(flightSection.Seats);
-            }
-            else
-            {
-                return false;
-            }
-
+            return obj is FlightSection flight &&
+                SeatClass == flight.SeatClass &&
+                Seats == flight.Seats;
         }
         public override int GetHashCode()
         {
-            int hashCode = -327950722;
-            hashCode = hashCode * -1521134295 + _seatClass.GetHashCode();
-            hashCode = hashCode * -1521134295 + EqualityComparer<List<Seat>>.Default.GetHashCode(_seats);
+            int hashCode = -1440401887;
             hashCode = hashCode * -1521134295 + SeatClass.GetHashCode();
             hashCode = hashCode * -1521134295 + EqualityComparer<List<Seat>>.Default.GetHashCode(Seats);
             return hashCode;
@@ -220,7 +109,7 @@ namespace AirlineBookingSystem
         #endregion
 
         #region Other Overridden Methods
-        public override string ToString() => $"{_seatClass} ALL: {_seats.Count} AVLBL: {CountAvailableSeats} OCCPD: {CountOccupiedSeats}"; 
+        public override string ToString() => $"{SeatClass} ALL: {Seats.Count} AVLBL: {CountAvailableSeats} OCCPD: {CountOccupiedSeats}";
         #endregion
     }
 }
