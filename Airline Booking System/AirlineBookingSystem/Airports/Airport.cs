6eaabbed52374e5108ed7602af9662bdb0ac1c43
kriyandi <kriyandimitrov@gmail.com>
2021-04-28 16:29:06 +0300

    Modify Airport class

--- Airline Booking System/AirlineBookingSystem/Airports/Airport.cs
+++ Airline Booking System/AirlineBookingSystem/Airports/Airport.cs
@@ -10,14 +10,10 @@ namespace AirlineBookingSystem
 {
     public class Airport
     {
-        #region Fields
-        private string _name;
-        #endregion
-
         #region Constructors
         public Airport(string name)
         {
-            InitializeDataMembers(name);
+            Name = name;
         }
         public Airport(Airport other) : this(other.Name)
         {
@@ -25,39 +21,14 @@ namespace AirlineBookingSystem
         #endregion
 
         #region Properties
-        public string Name => _name;
-        #endregion
-
-        #region Methods
-        public AirportOperation ChangeName(string airportName)
-        {
-            AirportOperation result = ValidationRules.AirportName(airportName);
-
-            switch (result)
-            {
-                case AirportOperation.Succeded:
-                    _name = airportName;
-                    break;
-                case AirportOperation.InvalidNameLenghtFailure:
-                    Console.WriteLine("Error: Airport name should be exact 3 letters long.");
-                    break;
-                case AirportOperation.InvalidNameNullFailure:
-                    Console.WriteLine("Error: Airport name can not be null.");
-                    break;
-                case AirportOperation.InvalidNameFormatFailure:
-                    Console.WriteLine("Error: Airport name should contain only capital letters.");
-                    break;
-            }
-
-            return result;
-        }
+        public string Name { get; set; }
         #endregion
 
         #region Equations Methods
         public override bool Equals(object obj)
         {
             return obj is Airport airport &&
-                   _name == airport.Name;
+                   Name == airport.Name;
         }
         public override int GetHashCode()
         {
@@ -66,25 +37,9 @@ namespace AirlineBookingSystem
         public static bool operator ==(Airport lhs, Airport rhs) => lhs.Equals(rhs);
         public static bool operator !=(Airport lhs, Airport rhs) => !(lhs == rhs);
         #endregion
-        
-        #region Help Methods
-        private void InitializeDataMembers(string name)
-        {
-            InitializeName(name);
-        }
-        private void InitializeName(string name)
-        {
-            if (ChangeName(name) != AirportOperation.Succeded)
-            {
-                Console.WriteLine("Airport was not created!");
-
-                throw new ArgumentException(AirportExceptionMessages.invalidName);
-            }
-        }
-        #endregion
 
         #region Other Overridden Methods
-        public override string ToString() => $"{_name}"; 
+        public override string ToString() => $"{Name}";
         #endregion
     }
 }
