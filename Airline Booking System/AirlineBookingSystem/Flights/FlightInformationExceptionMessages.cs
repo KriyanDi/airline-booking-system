cf5d55eff74274c300f924062c201309c4910a96
kriyandi <kriyandimitrov@gmail.com>
2021-04-28 10:32:29 +0300

    Add FlightOperationExceptionmessages class

---
+++ Airline Booking System/AirlineBookingSystem/Flights/FlightInformationExceptionMessages.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirlineBookingSystem.Flights
+{
+    class FlightInformationExceptionMessages
+    {
+        public static string invalidFlightNumber = "Invalid Flight Number!";
+    }
+}
