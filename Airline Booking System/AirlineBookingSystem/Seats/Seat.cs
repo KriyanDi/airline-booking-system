e04c17898bfa90c9829ae254309d12a881c2259a
kriyandi <kriyandimitrov@gmail.com>
2021-05-05 12:20:55 +0300

    Modify Seat class

--- Airline Booking System/AirlineBookingSystem/Seats/Seat.cs
+++ Airline Booking System/AirlineBookingSystem/Seats/Seat.cs
@@ -14,22 +14,6 @@
         public (int rows, char cols) Id { get; set; }
         public bool IsBooked { get; set; }
 
-        public override bool Equals(object obj)
-        {
-            return obj is Seat seat &&
-                   Id == seat.Id &&
-                   IsBooked == seat.IsBooked;
-        }
-        public override int GetHashCode()
-        {
-            int hashCode = -1850583607;
-            hashCode = hashCode * -1521134295 + Id.GetHashCode();
-            hashCode = hashCode * -1521134295 + IsBooked.GetHashCode();
-            return hashCode;
-        }
-        public static bool operator ==(Seat lhs, Seat rhs) => lhs.Equals(rhs);
-        public static bool operator !=(Seat lhs, Seat rhs) => !(lhs == rhs);
-
         public override string ToString() => $"ROW: {Id.rows} COL: {Id.cols} BOOKED: {IsBooked}";
     }
 }
