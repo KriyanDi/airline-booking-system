475d03df2ca23438d3d3d3d8968213bd7a8d2c89
kriyandi <kriyandimitrov@gmail.com>
2021-05-05 12:20:21 +0300

    Modify Airport class

--- Airline Booking System/AirlineBookingSystem/Airports/Airport.cs
+++ Airline Booking System/AirlineBookingSystem/Airports/Airport.cs
@@ -12,18 +12,6 @@
 
         public string Name { get; set; }
 
-        public override bool Equals(object obj)
-        {
-            return obj is Airport airport &&
-                   Name == airport.Name;
-        }
-        public override int GetHashCode()
-        {
-            return base.GetHashCode();
-        }
-        public static bool operator ==(Airport lhs, Airport rhs) => lhs.Equals(rhs);
-        public static bool operator !=(Airport lhs, Airport rhs) => !(lhs == rhs);
-
         public override string ToString() => $"{Name}";
     }
 }
