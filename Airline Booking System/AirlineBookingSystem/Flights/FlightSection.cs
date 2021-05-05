519cf839f2a4069ca7305b3b4f93f4063a566b8d
kriyandi <kriyandimitrov@gmail.com>
2021-05-05 22:44:38 +0300

    Modify FlightSection class methods

--- Airline Booking System/AirlineBookingSystem/Flights/FlightSection.cs
+++ Airline Booking System/AirlineBookingSystem/Flights/FlightSection.cs
@@ -18,52 +18,85 @@ namespace AirlineBookingSystem
         #region Properties
         public SeatClass SeatClass { get; set; }
         [SeatSection]
-        public List<Seat> Seats { get; set; }
-        public int CountAvailableSeats => Seats.Count(seat => !seat.IsBooked);
-        public int CountOccupiedSeats => Seats.Count - CountAvailableSeats;
+        public Seat[,] Seats { get; set; }
+        public int CountAvailableSeats
+        {
+            get
+            {
+                int sum = 0;
+
+                for (int i = 0; i < Seats.GetLength(0); i++)
+                {
+                    for (int j = 0; j < Seats.GetLength(1); j++)
+                    {
+                        if (!Seats[i, j].IsBooked)
+                        {
+                            sum++;
+                        }
+                    }
+                }
+
+                return sum;
+            }
+        }
+        public int CountOccupiedSeats => Seats.Length - CountAvailableSeats;
         #endregion
 
         #region Methods
-        public bool HasAvailableSeats() => Seats.Any(seat => !seat.IsBooked);
-        public bool BookSeat(int rows, char cols)
+        public bool HasAvailableSeats()
         {
-            for (int i = 0; i < Seats.Count; i++)
+            for (int i = 0; i < Seats.GetLength(0); i++)
             {
-                if (Seats[i].Id == (rows, cols))
+                for (int j = 0; j < Seats.GetLength(1); j++)
                 {
-                    if (!Seats[i].IsBooked)
+                    if (!Seats[i, j].IsBooked)
                     {
-                        Seats[i].IsBooked = true;
                         return true;
-                    }
-                    else
-                    {
-                        break;
                     }
                 }
             }
 
             return false;
         }
+        public bool BookSeat(int rows, char cols)
+        {
+            try
+            {
+                if (!Seats[rows - 1, cols - 'A'].IsBooked)
+                {
+                    Seats[rows - 1, cols - 'A'].IsBooked = true;
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
         #endregion
 
         #region Help Methods
         private void InitializeSeats(int rows, int cols)
         {
-            Seats = new List<Seat>();
+            Seats = new Seat[rows, cols];
 
             for (int i = 0; i < rows; i++)
             {
                 for (int j = 0; j < cols; j++)
                 {
-                    Seats.Add(new Seat((i + 1, Convert.ToChar(65 + (j % cols))), false));
+                    Seats[i, j] = new Seat((i + 1, Convert.ToChar(65 + (j % cols))), false);
                 }
             }
         }
         #endregion
 
         #region Other Overridden Methods
-        public override string ToString() => $"{SeatClass} ALL: {Seats.Count} AVLBL: {CountAvailableSeats} OCCPD: {CountOccupiedSeats}";
+        public override string ToString() => $"{SeatClass} ALL: {Seats.Length} AVLBL: {CountAvailableSeats} OCCPD: {CountOccupiedSeats}";
         #endregion
     }
 }
