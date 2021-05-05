1dbfa4245ca1fcab77befa8d4968ca94655bf4dc
kriyandi <kriyandimitrov@gmail.com>
2021-05-05 22:43:32 +0300

    Modify ValidationAttributes file

--- Airline Booking System/AirlineBookingSystem/Additional/ValidationAttributes.cs
+++ Airline Booking System/AirlineBookingSystem/Additional/ValidationAttributes.cs
@@ -1,31 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Text.RegularExpressions;
 
 namespace AirlineBookingSystem.Additional
 {
-    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property | AttributeTargets.Parameter)]
     sealed public class AirportName : ValidationAttribute
     {
         public AirportName() => ErrorMessage = "Airport name should be exact three capital letters long.";
         public override bool IsValid(object value) => (value is string str && Regex.IsMatch(str, @"^[A-Z]{3}$"));
     }
-
-    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property | AttributeTargets.Parameter)]
     sealed public class AirlineName : ValidationAttribute
     {
         public AirlineName() => ErrorMessage = "Airline name should be with more than zero and less than six alphanumeric capital symbols long.";
         public override bool IsValid(object value) => (value is string str && Regex.IsMatch(str, @"^[A-Z0-9]{1,5}$"));
     }
-
-    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property | AttributeTargets.Parameter)]
     sealed public class FlightNumber : ValidationAttribute
     {
         public FlightNumber() => ErrorMessage = "Flight number should contain only numerics.";
         public override bool IsValid(object value) => (value is string str && Regex.IsMatch(str, @"^[0-9]+$"));
     }
-
-    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property | AttributeTargets.Parameter)]
     sealed public class SeatSection : ValidationAttribute
     {
         public SeatSection() => ErrorMessage = "Seat row should be number between 1 - 100 and seat column should be between 1 - 10";
@@ -34,10 +28,8 @@ namespace AirlineBookingSystem.Additional
             // Try-catch is used instead of "value is (int,char)" because C# 7.3 does not support this
             try
             {
-                (int row, int col) info = ((int, int))value;
-
-                return 0 <= info.row && info.row <= 100 &&
-                    1 <= info.col && info.col <= 10;
+                return 1 <= ((Seat[,])value).GetLength(0) && ((Seat[,])value).GetLength(0) <= 100 &&
+                     1 <= ((Seat[,])value).GetLength(1) && ((Seat[,])value).GetLength(1) <= 10;
             }
             catch (Exception ex)
             {
