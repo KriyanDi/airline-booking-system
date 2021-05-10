262e470e727935cd398c1945543a04e4114b74a5
kriyandi <kriyandimitrov@gmail.com>
2021-05-10 14:10:01 +0300

    Add ISystemManageableExtend interface

---
+++ Airline Booking System/AirlineBookingSystem/SystemManagers/ISystemManageableExtend.cs
@@ -0,0 +1,12 @@
+﻿using AirlineBookingSystem.Additional;
+
+namespace AirlineBookingSystem.SystemManagers
+{
+    interface ISystemManageableExtend : ISystemManageable
+    {
+        OperationResult CreateAirport(string airportName);
+        OperationResult CreateAirline(string airlineName);
+        OperationResult CreateFlight(string airlineName, string fromAirport, string toAirport, int year, int month, int day, string id);
+        OperationResult CreateSection(string airlineName, string flightId, int rows, int cols, SeatClass seatClass);
+    }
+}
