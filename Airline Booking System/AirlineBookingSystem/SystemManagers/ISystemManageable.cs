cdc37f1d0793d4cda058b013a3ed308750b58bcb
kriyandi <kriyandimitrov@gmail.com>
2021-05-10 14:09:22 +0300

    Modify ISystemManageable

--- Airline Booking System/AirlineBookingSystem/SystemManagers/ISystemManageable.cs
+++ Airline Booking System/AirlineBookingSystem/SystemManagers/ISystemManageable.cs
@@ -5,10 +5,10 @@ namespace AirlineBookingSystem
 {
     public interface ISystemManageable
     {
-        OperationResult CreateAirport(string airportName);
-        OperationResult CreateAirline(string airlineName);
-        OperationResult CreateFlight(string airlineName, string fromAirport, string toAirport, int year, int month, int day, string id);
-        OperationResult CreateSection(string airlineName, string flightId, int rows, int cols, SeatClass seatClass);
+        OperationResult CreateAirport(Airport airport);
+        OperationResult CreateAirline(Airline airline);
+        OperationResult CreateFlight(Flight flight);
+        OperationResult CreateSection(string airlineName, string flightId, FlightSection section);
         List<Flight> FindAvailableFlights(string originatingAirport, string destionationAirport);
         OperationResult BookSeat(string airlineName, string flightNumber, SeatClass seatClass, int rows, char cols);
         void DisplaySystemDetails();
