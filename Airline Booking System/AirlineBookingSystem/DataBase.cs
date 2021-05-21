6564b43eb385ff447c2e56115c61fcd4441dec13
kriyandi <kriyandimitrov@gmail.com>
2021-05-21 15:59:49 +0300

    Add data base

---
+++ Airline Booking System/AirlineBookingSystem/DataBase.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AirlineBookingSystem
+{
+    public class DataBase
+    {
+        public Dictionary<string, Airport> Airports
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public Dictionary<string, Airline> Airlines
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public Dictionary<string, Flight> Flights
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public void AddElement()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
