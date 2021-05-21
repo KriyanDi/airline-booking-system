d3fc920cddc0595d263072a09c408243d7338072
kriyandi <kriyandimitrov@gmail.com>
2021-05-21 15:59:39 +0300

    Add validate object

---
+++ Airline Booking System/AirlineBookingSystem/ValidateObject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+
+namespace AirlineBookingSystem
+{
+    public class ValidateObject
+    {
+        public static bool TryValidate(object obj)
+        {
+            var result = new List<ValidationResult>();
+            bool isValid = Validator.TryValidateObject(obj, new ValidationContext(obj), result, true);
+            foreach (var res in result)
+            {
+                Console.WriteLine(res);
+            }
+            return isValid;
+        }
+    }
+}
