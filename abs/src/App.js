1e4d2a526363dc6616804a617fc7d5a9cf302e91
kriyandi <kriyandimitrov@gmail.com>
2021-07-13 16:06:28 +0300

    Modify App component

--- abs/src/App.js
+++ abs/src/App.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import AddAirline from './components/AddAirline';
-import AddAirport from './components/AddAirport';
+import React from "react";
+import AddAirline from "./components/AddAirline";
+import AddAirport from "./components/AddAirport";
+import AddFlight from "./components/AddFlight";
 
 const App = () => {
-    return (
-        <div className="ui form attached segment">
-            <AddAirport />
-            <AddAirline />
-        </div>
-    );
-}
+  return (
+    <div className="ui form attached segment">
+      <div className="ui segment">
+        <h2 class="ui dividing header container">Airline Booking System ✈️</h2>
+        <AddAirport />
+        <AddAirline />
+        <AddFlight />
+      </div>
+    </div>
+  );
+};
 
 export default App;
