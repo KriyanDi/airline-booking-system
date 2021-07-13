72ad7752b55411496676d1f741d0503250bdbed4
kriyandi <kriyandimitrov@gmail.com>
2021-07-13 14:22:43 +0300

    Add AddAirline component

---
+++ abs/src/components/AddAirline.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import AddField from './AddField';
+
+const AddAirline = (props) => {
+    return (
+        <AddField objectName="Airline" buttonName="Airline" />
+    );
+}
+
+export default AddAirline;
