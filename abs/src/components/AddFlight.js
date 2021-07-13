3b5ee71a67d07c7b6f398e2e005c308721050a96
kriyandi <kriyandimitrov@gmail.com>
2021-07-13 16:06:15 +0300

    Add AddFlight component

---
+++ abs/src/components/AddFlight.js
@@ -0,0 +1,31 @@
+import React from "react";
+import DropdownWithLabel from "./common/DropdownWithLabel";
+import TextInputWithLabel from "./common/TextInputWithLabel";
+
+const AddFlight = (props) => {
+  return (
+    <div className="ui form field segment container">
+      <h4 class="ui dividing header">Manage Flights</h4>
+      <div className="three fields">
+        <DropdownWithLabel label="Airline Name:" />
+        <DropdownWithLabel label="From Airport:" />
+        <DropdownWithLabel label="Destination Airport:" />
+      </div>
+
+      <div className="three fields">
+        <DropdownWithLabel label="Section:" />
+        <TextInputWithLabel label={"Number of Rows:"} placeholder={"Rows ..."} />
+        <TextInputWithLabel label={"Number of Cols:"} placeholder={"Cols ..."} />
+      </div>
+
+      <div className="ui segment">
+        <label>Current Sections:</label>
+        {/* SectionsList */}
+      </div>
+
+      <button className="ui button">Add Flight</button>
+    </div>
+  );
+};
+
+export default AddFlight;
