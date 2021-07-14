8c051d7ccfe17948763639df3e1b7059758e7804
kriyandi <kriyandimitrov@gmail.com>
2021-07-14 12:36:05 +0300

    Modify App component

--- abs/src/App.js
+++ abs/src/App.js
@@ -2,17 +2,26 @@ import React from "react";
 import AddAirline from "./components/AddAirline";
 import AddAirport from "./components/AddAirport";
 import AddFlight from "./components/AddFlight";
+import BookSeat from "./components/BookSeat";
+
+import { Provider } from "react-redux";
+import store from "./store";
 
 const App = () => {
   return (
-    <div className="ui form attached segment">
-      <div className="ui segment">
-        <h2 class="ui dividing header container">Airline Booking System ✈️</h2>
-        <AddAirport />
-        <AddAirline />
-        <AddFlight />
+    <Provider store={store}>
+      <div className="ui form attached segment">
+        <div className="ui segment">
+          <h2 className="ui dividing header container">
+            Airline Booking System ✈️
+          </h2>
+          <AddAirport />
+          <AddAirline />
+          <AddFlight />
+          <BookSeat />
+        </div>
       </div>
-    </div>
+    </Provider>
   );
 };
 
