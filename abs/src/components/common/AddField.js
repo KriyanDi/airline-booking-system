ba975b93bba7b0ad9ea40bef0330251db73e7f6f
kriyandi <kriyandimitrov@gmail.com>
2021-07-14 12:39:53 +0300

    Modify AddField component

--- abs/src/components/common/AddField.js
+++ abs/src/components/common/AddField.js
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
-const AddField = ({ objectName, buttonName }) => {
+const AddField = ({ objectName, buttonName, onClick }) => {
+  const [value, setValue] = useState("");
   return (
-    <div className="ui segment container">
-      <div className="ui form field =">
-        <h4 class="ui dividing header">Manage {objectName}</h4>
-        <div className="ui action input">
-          <input type="text" placeholder={`${objectName} ...`} />
-          <button className="ui button">{`Add ${buttonName}`}</button>
-        </div>
+    <div className="ui form field =">
+      <h4 className="ui dividing header">Manage {objectName}</h4>
+      <div className="ui action input">
+        <input
+          type="text"
+          placeholder={`${objectName} ...`}
+          onChange={(event) => setValue(event.target.value)}
+          value={value}
+        />
+        <button
+          className="ui button"
+          onClick={() => {
+            onClick(value);
+            setValue("");
+          }}
+        >{`Add ${buttonName}`}</button>
       </div>
     </div>
   );
