a5de59f3140f051627cfa40a98ab2346832ad6ba
kriyandi <kriyandimitrov@gmail.com>
2021-07-14 12:38:55 +0300

    Modify and subscribe AddAirport component to store

--- abs/src/components/AddAirport.js
+++ abs/src/components/AddAirport.js
@@ -1,8 +1,26 @@
 import React from "react";
-import AddField from "./common/AddField";
+import { connect } from "react-redux";
+import { createAirport, deleteAirport } from "../actions/airportActions";
+import { selectAirportList } from "../selectors";
+import WrapAddFieldListViewer from "./common/WrapAddFieldListViewer";
 
 const AddAirport = (props) => {
-  return <AddField objectName="Airport" buttonName="Airport" />;
+  return (
+    <WrapAddFieldListViewer
+      objectName="Airport"
+      buttonName="Airport"
+      onAddClick={props.createAirport}
+      onDeleteClick={props.deleteAirport}
+      list={props.airports}
+    />
+  );
 };
 
-export default AddAirport;
+const mapStateToProps = (state) => {
+  const airports = selectAirportList(state);
+  return { airports };
+};
+
+export default connect(mapStateToProps, { createAirport, deleteAirport })(
+  AddAirport
+);
