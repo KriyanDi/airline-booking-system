3a843f1651c4c2ed5d93699735e02c485250026a
kriyandi <kriyandimitrov@gmail.com>
2021-07-14 12:39:22 +0300

    Add AddFlight component

--- abs/src/components/AddFlight.js
+++ abs/src/components/AddFlight.js
@@ -5,7 +5,7 @@ import TextInputWithLabel from "./common/TextInputWithLabel";
 const AddFlight = (props) => {
   return (
     <div className="ui form field segment container">
-      <h4 class="ui dividing header">Manage Flights</h4>
+      <h4 className="ui dividing header">Manage Flights</h4>
       <div className="three fields">
         <DropdownWithLabel label="Airline Name:" />
         <DropdownWithLabel label="From Airport:" />
@@ -14,8 +14,14 @@ const AddFlight = (props) => {
 
       <div className="three fields">
         <DropdownWithLabel label="Section:" />
-        <TextInputWithLabel label={"Number of Rows:"} placeholder={"Rows ..."} />
-        <TextInputWithLabel label={"Number of Cols:"} placeholder={"Cols ..."} />
+        <TextInputWithLabel
+          label={"Number of Rows:"}
+          placeholder={"Rows ..."}
+        />
+        <TextInputWithLabel
+          label={"Number of Cols:"}
+          placeholder={"Cols ..."}
+        />
       </div>
 
       <div className="ui segment">
