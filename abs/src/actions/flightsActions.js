1233554b060e62c21c38072256a1736ab0b022f8
kriyandi <kriyandimitrov@gmail.com>
2021-07-14 16:48:59 +0300

    Add flight actions

---
+++ abs/src/actions/flightsActions.js
@@ -0,0 +1,56 @@
+import { FLIGHT } from "./actionTypes";
+
+export const createFlight = (airline, from, to, seatClass, cols, rows) => ({
+  type: FLIGHT.ADD_FLIGHT,
+  payload: {
+    airline,
+    from,
+    to,
+    seatClass,
+    cols,
+    rows,
+  },
+});
+
+export const deleteFlight = (id) => ({
+  type: FLIGHT.DELETE_FLIGHT,
+  payload: {
+    id,
+  },
+});
+
+export const createSection = (id, seatClass, rows, cols) => ({
+  type: FLIGHT.ADD_SECTION,
+  payload: {
+    id,
+    seatClass,
+    rows,
+    cols,
+  },
+});
+
+export const deleteSection = (id, seatClass) => ({
+  type: FLIGHT.DELETE_SECTION,
+  payload: {
+    id,
+    seatClass,
+  },
+});
+
+export const bookSeat = (id, seatClass, seatId) => ({
+  type: FLIGHT.BOOK_SEAT,
+  payload: {
+    id,
+    seatClass,
+    seatId,
+  },
+});
+
+export const unbookSeat = (id, seatClass, seatId) => ({
+  type: FLIGHT.UNBOOK_SEAT,
+  payload: {
+    id,
+    seatClass,
+    seatId,
+  },
+});
