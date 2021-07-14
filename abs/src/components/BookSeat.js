f242685db9bd61a104d41adc4093a7a3ec091843
kriyandi <kriyandimitrov@gmail.com>
2021-07-14 15:41:06 +0300

    Modify and connect BookSeat to the store

--- abs/src/components/BookSeat.js
+++ abs/src/components/BookSeat.js
@@ -1,15 +1,18 @@
 import React from "react";
 import DropdownWithLabel from "./common/DropdownWithLabel";
 import TextInputWithLabel from "./common/TextInputWithLabel";
+import { connect } from "react-redux";
+import { selectAirportList, selectAirlineList } from "../selectors";
+import { SEATCLASS } from "../constants";
 
 const BookSeat = (props) => {
   return (
     <div className="ui form field segment container">
       <h4 className="ui dividing header">Manage Book Seats</h4>
       <div className="three fields">
-        <DropdownWithLabel label="Airline Name:" />
-        <DropdownWithLabel label="Flight Number:" />
-        <DropdownWithLabel label="Seat class:" />
+        <DropdownWithLabel label="Airline Name:" list={props.airlines} />
+        <DropdownWithLabel label="Flight Number:" list={props.flighNumbers} />
+        <DropdownWithLabel label="Seat class:" list={Object.keys(SEATCLASS)} />
       </div>
       <div className="two fields">
         <TextInputWithLabel label="Row:" placeholder="Row ..." />
@@ -21,4 +24,11 @@ const BookSeat = (props) => {
   );
 };
 
-export default BookSeat;
+const mapStateToProps = (state) => {
+  const airports = selectAirportList(state).map((el) => el.name);
+  const airlines = selectAirlineList(state).map((el) => el.name);
+
+  return { airports, airlines };
+};
+
+export default connect(mapStateToProps)(BookSeat);
