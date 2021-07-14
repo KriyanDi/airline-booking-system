e5df0715083ea41ab30e269bd097771aad66578a
kriyandi <kriyandimitrov@gmail.com>
2021-07-14 15:40:46 +0300

    Modify and connect AddFlight component to the store

--- abs/src/components/AddFlight.js
+++ abs/src/components/AddFlight.js
@@ -1,19 +1,22 @@
 import React from "react";
 import DropdownWithLabel from "./common/DropdownWithLabel";
 import TextInputWithLabel from "./common/TextInputWithLabel";
+import { connect } from "react-redux";
+import { selectAirportList, selectAirlineList } from "../selectors";
+import { SEATCLASS } from "../constants";
 
 const AddFlight = (props) => {
   return (
     <div className="ui form field segment container">
       <h4 className="ui dividing header">Manage Flights</h4>
       <div className="three fields">
-        <DropdownWithLabel label="Airline Name:" />
-        <DropdownWithLabel label="From Airport:" />
-        <DropdownWithLabel label="Destination Airport:" />
+        <DropdownWithLabel label="Airline Name:" list={props.airlines} />
+        <DropdownWithLabel label="From Airport:" list={props.airports} />
+        <DropdownWithLabel label="Destination Airport:" list={props.airports} />
       </div>
 
       <div className="three fields">
-        <DropdownWithLabel label="Section:" />
+        <DropdownWithLabel label="Section:" list={Object.keys(SEATCLASS)} />
         <TextInputWithLabel
           label={"Number of Rows:"}
           placeholder={"Rows ..."}
@@ -34,4 +37,11 @@ const AddFlight = (props) => {
   );
 };
 
-export default AddFlight;
+const mapStateToProps = (state) => {
+  const airports = selectAirportList(state).map((el) => el.name);
+  const airlines = selectAirlineList(state).map((el) => el.name);
+
+  return { airports, airlines };
+};
+
+export default connect(mapStateToProps)(AddFlight);
