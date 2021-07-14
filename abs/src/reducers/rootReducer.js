f489f602eb868f35af9c63924934c5929f13b81b
kriyandi <kriyandimitrov@gmail.com>
2021-07-14 13:05:32 +0300

    Add airlineReducer to rootReducer

--- abs/src/reducers/rootReducer.js
+++ abs/src/reducers/rootReducer.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 
 import airportReducer from "./airport";
+import airlineReducer from "./airline";
 
 const rootReducer = combineReducers({
   airportReducer,
+  airlineReducer,
 });
 
 export default rootReducer;
