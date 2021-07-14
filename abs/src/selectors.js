4ed99ac63bb80ef5676d28879a4ab0e6f43db419
kriyandi <kriyandimitrov@gmail.com>
2021-07-14 13:20:38 +0300

    Add airlineList selector

--- abs/src/selectors.js
+++ abs/src/selectors.js
@@ -2,3 +2,8 @@ export const selectAirportList = (store) =>
   store && store.airportReducer
     ? Array.from(store.airportReducer.airports.values())
     : [];
+
+export const selectAirlineList = (store) =>
+  store && store.airlineReducer
+    ? Array.from(store.airlineReducer.airlines.values())
+    : [];
