565d448eb5227081fbfe85ed5c480938242fa6df
kriyandi <kriyandimitrov@gmail.com>
2021-07-15 13:14:22 +0300

    Modify AddFlight component

--- abs/src/components/Flight/AddFlight.js
+++ abs/src/components/Flight/AddFlight.js
@@ -1,31 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import DropdownWithLabel from "../_common/DropdownWithLabel";
 import { connect } from "react-redux";
-import { selectAirportList, selectAirlineList } from "../../utils/selectors";
-import AddSection from "./AddSection";
+import {
+  selectAirportList,
+  selectAirlineList,
+  selectFlightList,
+} from "../../utils/selectors";
+import { createFlight, deleteFlight } from "../../actions/flightsActions";
+import TableViewer from "../_common/TableViewer";
 
 const AddFlight = (props) => {
+  const [airline, setAirline] = useState("");
+  const [from, setFrom] = useState("");
+  const [to, setTo] = useState("");
+
   return (
     <div className="ui form field segment container">
       <h4 className="ui dividing header">Manage Flights</h4>
       <div className="ui segment">
         <h5 className="ui dividing header">Add Flights</h5>
         <div className="three fields">
-          <DropdownWithLabel label="Airline Name:" list={props.airlines} />
-          <DropdownWithLabel label="From Airport:" list={props.airports} />
+          <DropdownWithLabel
+            label="Airline Name:"
+            list={props.airlines}
+            onChange={setAirline}
+          />
+          <DropdownWithLabel
+            label="From Airport:"
+            list={props.airports}
+            onChange={setFrom}
+          />
           <DropdownWithLabel
             label="Destination Airport:"
             list={props.airports}
+            onChange={setTo}
           />
         </div>
-        <button className="ui button">Add Flight</button>
+        <button
+          className="ui button"
+          onClick={() => {
+            if (airline !== "" && from !== "" && to !== "") {
+              props.createFlight(airline, from, to);
+            }
+          }}
+        >
+          Add Flight
+        </button>
       </div>
 
-      <AddSection />
-
       <div className="ui segment">
-        <label>Current Sections:</label>
-        {/* SectionsList */}
+        <h4 className="ui dividing grey header">Flights</h4>
+        <TableViewer content={props.flights} onClick={props.deleteFlight} />
       </div>
     </div>
   );
@@ -34,8 +59,11 @@ const AddFlight = (props) => {
 const mapStateToProps = (state) => {
   const airports = selectAirportList(state).map((el) => el.name);
   const airlines = selectAirlineList(state).map((el) => el.name);
+  const flights = selectFlightList(state);
 
-  return { airports, airlines };
+  return { airports, airlines, flights };
 };
 
-export default connect(mapStateToProps)(AddFlight);
+export default connect(mapStateToProps, { createFlight, deleteFlight })(
+  AddFlight
+);
