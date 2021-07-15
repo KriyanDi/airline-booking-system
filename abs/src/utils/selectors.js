8749118f53610ff9de6e829c37b9518615d20803
kriyandi <kriyandimitrov@gmail.com>
2021-07-15 13:15:27 +0300

    Add selectors for flight

--- abs/src/utils/selectors.js
+++ abs/src/utils/selectors.js
@@ -7,3 +7,14 @@ export const selectAirlineList = (store) =>
   store && store.airlineReducer
     ? Array.from(store.airlineReducer.airlines.values())
     : [];
+
+export const selectFlightList = (store) =>
+  store && store.flightReducer
+    ? Array.from(store.flightReducer.flights.values())
+    : [];
+
+export const selectFlightById = (store, id) =>
+  selectAirlineList(store).Find((el) => el.id === id);
+
+export const selectFlightSeatClasses = (store, id) =>
+  selectFlightById(store, id).seatClasses;
