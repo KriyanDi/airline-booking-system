e7cf2c25874f783f8bb0ad2da298284342102676
kriyandi <kriyandimitrov@gmail.com>
2021-07-19 10:26:35 +0300

    Modify SearchFlight component

--- abs/src/components/Flight/SearchFlight.js
+++ abs/src/components/Flight/SearchFlight.js
@@ -6,10 +6,17 @@ const SearchFlight = (props) => {
   const [from, setFrom] = useState("");
   const [to, setTo] = useState("");
 
-  const createSearchContent = (list) =>
-    list && list.lengt
-      ? list.filter((el) => el.from === from && el.to === to)
-      : [];
+  const createSearchContent = (from, to, list) => {
+    let searchedFlights =
+      list && list.length
+        ? list.filter((el) => el.from === from && el.to === to)
+        : [];
+
+    return searchedFlights.map((el) => ({
+      ...el,
+      seatClasses: el.seatClasses.length,
+    }));
+  };
 
   const header = "Search Flights";
 
@@ -19,20 +26,20 @@ const SearchFlight = (props) => {
       <div className="two fields">
         <DropdownWithLabel
           label="From Airport"
-          list={props.aiports}
+          list={props.airports}
           onChange={setFrom}
         />
 
         <DropdownWithLabel
           label="To Airport"
-          list={props.aiports}
+          list={props.airports}
           onChange={setTo}
         />
       </div>
 
       <div className="ui segment">
         <h4 className="ui dividing grey header">Search Results:</h4>
-        <TableViewer content={createSearchContent(props.flight)} />
+        <TableViewer content={createSearchContent(from, to, props.flights)} />
       </div>
     </div>
   );
