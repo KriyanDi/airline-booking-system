520f54d0fe6fdce193d3e91063e801d72527a1a5
kriyandi <kriyandimitrov@gmail.com>
2021-07-19 16:08:28 +0300

    Modify ShowSeats component
    Added function that creates seats content

--- abs/src/components/Flight/ShowSeats.js
+++ abs/src/components/Flight/ShowSeats.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 const ShowSeats = (props) => {
-  return <div className="ui segment">ShowSeats</div>;
+  const createSeatsContent = () => {
+    let seats = Array.from(props.seats.values());
+
+    return seats.map((el) => (
+      <div className="ui">{`${el.seat}  -  ${el.isBooked}`}</div>
+    ));
+  };
+
+  return <div className="ui segment">{createSeatsContent()}</div>;
 };
 
 export default ShowSeats;
