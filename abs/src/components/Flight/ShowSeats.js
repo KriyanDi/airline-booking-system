927186a1eddf9ad46e9be6f873752dbcc070c1fc
kriyandi <kriyandimitrov@gmail.com>
2021-07-20 16:24:12 +0300

    Modify ShowSeats
    Now seats are shown as a table

--- abs/src/components/Flight/ShowSeats.js
+++ abs/src/components/Flight/ShowSeats.js
@@ -1,12 +1,20 @@
 import React from "react";
+import TableViewer from "../_common/TableViewer";
 
 const ShowSeats = (props) => {
   const createSeatsContent = () => {
     let seats = Array.from(props.seats.values());
+    let content = seats.map((el) => ({
+      seatId: el.seatId,
+      booked: el.isBooked,
+    }));
+    //
 
-    return seats.map((el) => (
-      <div className="ui">{`${el.seat}  -  ${el.isBooked}`}</div>
-    ));
+    // return seats.map((el) => (
+    //   <div className="ui">{`${el.seat}  -  ${el.isBooked}`}</div>
+    // ));
+
+    return <TableViewer content={seats} />;
   };
 
   return <div className="ui segment">{createSeatsContent()}</div>;
