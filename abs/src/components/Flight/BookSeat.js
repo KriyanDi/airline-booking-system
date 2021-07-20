36f01f4b077d3b7d5bf3d8c2d14d860c89b4db14
kriyandi <kriyandimitrov@gmail.com>
2021-07-20 16:01:15 +0300

    Modify BookSeat component

--- abs/src/components/Flight/BookSeat.js
+++ abs/src/components/Flight/BookSeat.js
@@ -2,15 +2,10 @@ import React, { useState } from "react";
 import DropdownWithLabel from "../_common/DropdownWithLabel";
 import ShowSeats from "./ShowSeats";
 import { colsListNumber, rowsList } from "../../utils/constants";
-import {
-  selectRowsForFlightSection,
-  selectColsForFlightSection,
-} from "../../utils/selectors";
 
 const BookSeat = (props) => {
   const header = "Book Seat";
 
-  const [airline, setAirline] = useState("");
   const [flightId, setFlightId] = useState({});
   const [seatClass, setSeatClass] = useState("");
   const [row, setRow] = useState(null);
@@ -31,47 +26,28 @@ const BookSeat = (props) => {
   };
 
   const resetValues = () => {
-    setAirline("");
-    setFlightId({});
-    setSeatClass("");
     setRow(null);
     setCol(null);
     setSetDefault(true);
   };
 
   let isBookSeatButtonEnabled =
-    airline !== "" &&
-    flightId !== "" &&
-    seatClass !== "" &&
-    row !== null &&
-    col !== null &&
-    props.getFlightById(flightId) !== undefined;
+    row !== null && col !== null && props.getFlightById(flightId) !== undefined;
 
   return (
     <div className="ui form field segment container">
       <h4 className="ui dividing header">{header}</h4>
 
-      <div className="three fields">
-        <DropdownWithLabel
-          label="Airline Name:"
-          list={props.airlines}
-          onChange={setAirline}
-          setDefault={setDefault}
-          setSetDefault={setSetDefault}
-        />
+      <div className="two fields">
         <DropdownWithLabel
           label="Flight Id:"
           list={props.flightIds}
           onChange={setFlightId}
-          setDefault={setDefault}
-          setSetDefault={setSetDefault}
         />
         <DropdownWithLabel
           label="Seat class:"
           list={extractSeatClasses(props.getFlightById(flightId))}
           onChange={setSeatClass}
-          setDefault={setDefault}
-          setSetDefault={setSetDefault}
         />
       </div>
 
@@ -96,7 +72,7 @@ const BookSeat = (props) => {
         className={`ui ${isBookSeatButtonEnabled ? "" : "disabled"} button`}
         onClick={() => {
           let flight = props.getFlightById(flightId);
-          props.onBook(flight.id, seatClass, Number(row) + Number(col));
+          props.onBook(flight.id, seatClass, row + col);
           resetValues();
         }}
       >
