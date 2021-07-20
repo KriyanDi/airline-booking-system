2696a60f5694e086ab2c516db4d82b5545a98551
kriyandi <kriyandimitrov@gmail.com>
2021-07-20 18:02:27 +0300

    Modify ShowSeats component
    Deleted code

--- abs/src/components/Flight/ShowSeats.js
+++ abs/src/components/Flight/ShowSeats.js
@@ -4,15 +4,6 @@ import TableViewer from "../_common/TableViewer";
 const ShowSeats = (props) => {
   const createSeatsContent = () => {
     let seats = Array.from(props.seats.values());
-    let content = seats.map((el) => ({
-      seatId: el.seatId,
-      booked: el.isBooked,
-    }));
-    //
-
-    // return seats.map((el) => (
-    //   <div className="ui">{`${el.seat}  -  ${el.isBooked}`}</div>
-    // ));
 
     return <TableViewer content={seats} />;
   };
