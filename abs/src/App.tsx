11d0669e1624060369bdbcee6301b2685f9949fb
kriyandi <kriyandimitrov@gmail.com>
2021-07-21 17:43:56 +0300

    Add App component

---
+++ abs/src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const App = () => {
+  let header = "Airline Booking System ✈️";
+
+  return (
+    <div className="ui form attached segment">
+      <div className="ui segment">
+        <h2 className="ui dividing header container">{header}</h2>
+      </div>
+    </div>
+  );
+};
+
+export default App;
