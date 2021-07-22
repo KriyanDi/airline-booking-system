83177deb882c2269a5a05a54d97439f42f538fd9
kriyandi <kriyandimitrov@gmail.com>
2021-07-22 13:56:55 +0300

    Modify ManageAirports component

--- abs/src/components/airports/ManageAirports.tsx
+++ abs/src/components/airports/ManageAirports.tsx
@@ -2,6 +2,12 @@ import React from "react";
 import { IAirport } from "../../interfaces/airportModel";
 import ManageElement from "../_common/ManageElement";
 
+import { connect } from "react-redux";
+
+import { selectAirports } from "../../utils/selectors";
+import { createAirport, deleteAirport } from "../../actions/airportActions";
+import { deleteFlightsOnDeletedAirport } from "../../actions/flightActions";
+
 interface ManageAirportsProps {
   airports: any[];
   createAirport(name: string): void;
@@ -25,4 +31,13 @@ const ManageAirports = (props: ManageAirportsProps) => {
   );
 };
 
-export default ManageAirports;
+const mapStateToProps = (state: any) => {
+  const airports = selectAirports(state);
+  return { airports };
+};
+
+export default connect(mapStateToProps, {
+  createAirport,
+  deleteAirport,
+  deleteFlightsOnDeletedAirport,
+})(ManageAirports);
