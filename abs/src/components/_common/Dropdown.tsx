90bf47b72237ef6030eae230b138889e1a5378a7
kriyandi <kriyandimitrov@gmail.com>
2021-07-22 12:27:26 +0300

    Add Dropdown component

---
+++ abs/src/components/_common/Dropdown.tsx
@@ -0,0 +1,39 @@
+import React, { ChangeEvent } from "react";
+
+interface DropdownProps {
+  label: string;
+  list: any[];
+  onChange(event: string): void; //fix this type
+  defaultOption?: boolean;
+  setDefaultOption?(value: boolean): void;
+}
+
+const Dropdown = ({ label, list, onChange, defaultOption, setDefaultOption }: DropdownProps) => {
+  defaultOption = defaultOption ? defaultOption : false;
+
+  return (
+    <div className="field">
+      <label>{label}</label>
+      <select
+        className="ui search dropdown"
+        onChange={(event) => {
+          onChange(event.target.value ? event.target.value : "");
+          setDefaultOption ? setDefaultOption(false) : (value: string) => {};
+        }}
+      >
+        <option selected={defaultOption} value=""></option>
+        {list && list.length
+          ? list.map((el: any, index: number) => {
+              return (
+                <option key={index} value={el}>
+                  {el}
+                </option>
+              );
+            })
+          : null}
+      </select>
+    </div>
+  );
+};
+
+export default Dropdown;
