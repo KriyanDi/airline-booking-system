b019811fbca406127d869543a4069c540b19f030
kriyandi <kriyandimitrov@gmail.com>
2021-07-22 13:56:14 +0300

    Modify App component

--- abs/src/App.tsx
+++ abs/src/App.tsx
@@ -1,5 +1,7 @@
 import React from "react";
-import ManageAirports from "./components/airport/ManageAirports";
+import ManageAirports from "./components/airports/ManageAirports";
+import ManageAirlines from "./components/airlines/ManageAirlines";
+import ManageFlights from "./components/flights/ManageFlights";
 
 import { Provider } from "react-redux";
 import store from "./store";
@@ -11,6 +13,8 @@ const App = () => {
         <div className="ui segment">
           <h2 className="ui dividing header container">"Airline Booking System ✈️"</h2>
           <ManageAirports />
+          <ManageAirlines />
+          <ManageFlights />
         </div>
       </div>
     </Provider>
