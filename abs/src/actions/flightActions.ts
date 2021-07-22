6d53d88520d58dbb9c21baa7b236437601f6fd99
kriyandi <kriyandimitrov@gmail.com>
2021-07-22 12:27:07 +0300

    Add flight actions

---
+++ abs/src/actions/flightActions.ts
@@ -0,0 +1,71 @@
+import { Action, FLIGHT } from "../interfaces/actionTypes";
+import { IFlight, ISeatClasses, SEATCLASS } from "../interfaces/flightModel";
+
+let counter = 0;
+
+// FLIGHT
+export const createFlight = (airline: string, from: string, to: string, date: Date): Action<FLIGHT, IFlight> => ({
+  type: "ADD_FLIGHT",
+  payload: {
+    id: counter++,
+    airline: airline,
+    from: from,
+    to: to,
+    date: date,
+  },
+});
+
+export const deleteFlight = (id: number): Action<FLIGHT, { id: number }> => ({
+  type: "DELETE_FLIGHT",
+  payload: {
+    id: id,
+  },
+});
+
+export const deleteFlightsOnDeleteAirprot = (name: string): Action<FLIGHT, { name: string }> => ({
+  type: "DELETE_FLIGHTS_ON_DELETED_AIRLINE",
+  payload: {
+    name: name,
+  },
+});
+
+export const deleteFlightsOnDeleteAirline = (name: string): Action<FLIGHT, { name: string }> => ({
+  type: "DELETE_FLIGHTS_ON_DELETED_AIRLINE",
+  payload: {
+    name: name,
+  },
+});
+
+// SECTIONS
+export const createSection = (
+  id: number,
+  seatClass: SEATCLASS,
+  rows: number,
+  cols: number
+): Action<FLIGHT, { id: number; seatClass: SEATCLASS; rows: number; cols: number }> => ({
+  type: "ADD_SECTION",
+  payload: {
+    id: id,
+    seatClass: seatClass,
+    rows: rows,
+    cols: cols,
+  },
+});
+
+export const deleteSection = (id: number, seatClass: SEATCLASS): Action<FLIGHT, { id: number; seatClass: SEATCLASS }> => ({
+  type: "DELETE_SECTION",
+  payload: {
+    id: id,
+    seatClass: seatClass,
+  },
+});
+
+//BOOK SEAT
+export const bookSeat = (id: number, seatClass: SEATCLASS, seatId: string): Action<FLIGHT, { id: number; seatClass: SEATCLASS; seatId: string }> => ({
+  type: "BOOK_SEAT",
+  payload: {
+    id: id,
+    seatClass: seatClass,
+    seatId: seatId,
+  },
+});
