d120eb03e0fc5a8c461b842ec0c3a3ef44629f52
kriyandi <kriyandimitrov@gmail.com>
2021-07-22 12:30:18 +0300

    Link App to the store

--- abs/src/App.tsx
+++ abs/src/App.tsx
@@ -1,14 +1,19 @@
 import React from "react";
+import ManageAirports from "./components/airport/ManageAirports";
+
+import { Provider } from "react-redux";
+import store from "./store";
 
 const App = () => {
-  let header = "Airline Booking System ✈️";
-
   return (
-    <div className="ui form attached segment">
-      <div className="ui segment">
-        <h2 className="ui dividing header container">{header}</h2>
+    <Provider store={store}>
+      <div className="ui form attached segment">
+        <div className="ui segment">
+          <h2 className="ui dividing header container">"Airline Booking System ✈️"</h2>
+          <ManageAirports />
+        </div>
       </div>
-    </div>
+    </Provider>
   );
 };
 
