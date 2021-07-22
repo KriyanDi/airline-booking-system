4315a3b3d819bce28e2440fa8e3e61c72b547ea2
kriyandi <kriyandimitrov@gmail.com>
2021-07-22 12:27:51 +0300

    Add TableViewer component

---
+++ abs/src/components/_common/TableViewer.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+
+interface TableViewerProps {
+  content: any;
+  onDelete?(el: any): void;
+}
+
+const getObjectKeys = (object: any) => (object ? Object.keys(object) : null);
+
+const setTableHead = (list: any) =>
+  list && list.length
+    ? list.map((el: any, index: number) => <th key={index}>{el.toUpperCase()}</th>)
+    : null;
+
+const setRowContent = (data: any) => {
+  let keys = Object.keys(data);
+  return keys.map((key, index) => <td key={index}>{data[`${key}`]}</td>);
+};
+
+const setTableContent = (content: any, onDelete?: (el: any) => void) =>
+  content && content.length
+    ? content.map((el: any, index: number) => (
+        <tr key={index}>
+          {setRowContent(el)}
+          {onDelete !== undefined ? (
+            <td key={index} className="right aligned">
+              <div className="right floated content">
+                <div
+                  className="ui red button"
+                  onClick={() => {
+                    onDelete(el);
+                  }}
+                >
+                  Delete
+                </div>
+              </div>
+            </td>
+          ) : null}
+        </tr>
+      ))
+    : null;
+
+const TableViewer = ({ content, onDelete }: TableViewerProps) => {
+  let keys = content ? getObjectKeys(content[0]) : null;
+
+  return (
+    <table className="ui unstackable table">
+      <thead>
+        <tr key="head">
+          {setTableHead(keys)}
+          <th key="button" className="right aligned"></th>
+        </tr>
+      </thead>
+      <tbody>{setTableContent(content, onDelete)}</tbody>
+    </table>
+  );
+};
+
+export default TableViewer;
