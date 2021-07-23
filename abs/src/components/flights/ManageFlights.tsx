a2cc7782e8e9bd90288f3484fa96f0ace59c5776
kriyandi <kriyandimitrov@gmail.com>
2021-07-23 17:46:15 +0300

    Modify ManageFlights component

--- abs/src/components/flights/ManageFlights.tsx
+++ abs/src/components/flights/ManageFlights.tsx
@@ -1,27 +1,16 @@
 import React from "react";
 import AddFlight from "./AddFlight";
+import AddSection from "./AddSection";
+import BookSeat from "./BookSeat";
+import SearchFlights from "./SearchFlights";
 
 const ManageFlights = () => {
   return (
     <div className="ui form field segment container">
       <AddFlight />
       <AddSection />
-      {/* <AddSection
-        flightIds={props.flightIds}
-        getFlightById={props.getFlightById}
-        createSection={props.createSection}
-        deleteSection={props.deleteSection}
-      />
-      <BookSeat
-        airlines={props.airlines}
-        flightIds={props.flightIds}
-        getFlightById={props.getFlightById}
-        onBook={props.bookSeat}
-        onUnbook={props.unbookSeat}
-        findCols={props.getColsForFlightSection}
-        findRows={props.getRowsForFlightSection}
-      />
-      <SearchFlight airports={props.airports} flights={props.flights} /> */}
+      <BookSeat />
+      <SearchFlights />
     </div>
   );
 };
