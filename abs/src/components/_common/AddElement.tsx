5b8f92ab33141a5b7c6b73e26cea19cc89da6d36
kriyandi <kriyandimitrov@gmail.com>
2021-07-28 10:06:36 +0300

    Modify AddElement component

--- abs/src/components/_common/AddElement.tsx
+++ abs/src/components/_common/AddElement.tsx
@@ -1,16 +1,12 @@
 import React from "react";
 import { useState } from "react";
-
-interface AddElementProps {
-  elementName: string;
-  onAdd(elementName: string): void;
-}
+import { AddElementProps } from "../../interfaces/propsInterfaces";
 
 const AddElement = ({ elementName, onAdd }: AddElementProps) => {
   const [inputValue, setInputValue] = useState<string>("");
 
   const onAddHandler = (): void => {
-    onAdd(inputValue);
+    onAdd({ name: inputValue });
     setInputValue("");
   };
 
@@ -19,7 +15,12 @@ const AddElement = ({ elementName, onAdd }: AddElementProps) => {
   return (
     <div className="ui form field =">
       <div className="ui action input">
-        <input type="text" value={inputValue} placeholder={`${elementName} ...`} onChange={(event) => setInputValue(event.target.value)} />
+        <input
+          type="text"
+          value={inputValue}
+          placeholder={`${elementName} ...`}
+          onChange={(event) => setInputValue(event.target.value)}
+        />
         <button className={`ui ${isAddInputValueEnabled} button`} onClick={() => onAddHandler()}>
           Add {elementName}
         </button>
