8040e65842bf55d0a2090177793cbe9d487a4efb
kriyandi <kriyandimitrov@gmail.com>
2021-07-28 10:09:21 +0300

    Modify index

--- abs/src/index.tsx
+++ abs/src/index.tsx
@@ -2,4 +2,7 @@ import React from "react";
 import { render } from "react-dom";
 import App from "./App";
 
+import { enableMapSet } from "immer";
+enableMapSet();
+
 render(<App />, document.getElementById("root"));
