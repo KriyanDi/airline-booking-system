c228bf922804637104483477f573a2e030e31569
kriyandi <kriyandimitrov@gmail.com>
2021-07-28 10:07:39 +0300

    Modify ManageAirports component

--- abs/src/components/airports/ManageAirports.tsx
+++ abs/src/components/airports/ManageAirports.tsx
@@ -1,39 +1,32 @@
 import React from "react";
-import { IAirport } from "../../interfaces/airportModel";
 import ManageElement from "../_common/ManageElement";
-
 import { connect } from "react-redux";
-
 import { selectAirports } from "../../utils/selectors";
-import { createAirport, deleteAirport } from "../../_redux/actions/airportActions";
-import { deleteFlightsOnDeletedAirport } from "../../_redux/actions/flightActions";
+import { IAirport } from "../../interfaces/airportModel";
+import { ManageAirportProps } from "../../interfaces/propsInterfaces";
+import { createAirport, deleteAirport } from "../../_redux/slices/airportsSlice";
+import { deleteFlightsOnDeletedAirport } from "../../_redux/slices/flightsSlice";
+import { RootState } from "../../_redux/store";
 
-interface ManageAirportsProps {
-  airports: any[];
-  createAirport(name: string): void;
-  deleteAirport(id: number): void;
-  deleteFlightsOnDeletedAirport(name: string | undefined): void;
-}
-
-const ManageAirports = (props: ManageAirportsProps) => {
+const ManageAirports = (props: ManageAirportProps) => {
   const onAirportDeleteHandler = (el: IAirport) => {
-    props.deleteAirport(el.id);
-    props.deleteFlightsOnDeletedAirport(el.name);
+    props.deleteAirport({ id: el.id });
+    props.deleteFlightsOnDeletedAirport({ name: el.name });
   };
 
   return (
     <ManageElement
       elementName="Airports"
-      list={props.airports}
+      list={props.airportValues}
       onAdd={props.createAirport}
       onDelete={onAirportDeleteHandler}
     />
   );
 };
 
-const mapStateToProps = (state: any) => {
-  const airports = selectAirports(state);
-  return { airports };
+const mapStateToProps = (state: RootState) => {
+  let airportValues = selectAirports(state);
+  return { airportValues };
 };
 
 export default connect(mapStateToProps, {
