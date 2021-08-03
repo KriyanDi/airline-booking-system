0662c9acba3b488bce46cde127f5c5dc5cdfc174
kriyandi <kriyandimitrov@gmail.com>
2021-08-03 11:24:40 +0300

    Add Web Api for Airports

---
+++ apis/AbsApis/AirportsApi/Models/Airport.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AirportsApi.Models
+{
+    public class Airport
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+    }
+}
