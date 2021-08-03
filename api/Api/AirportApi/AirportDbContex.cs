d76abd6be3fd130fdf63f9abfab56153c181bc94
kriyandi <kriyandimitrov@gmail.com>
2021-08-03 13:32:58 +0300

    Add Airport API

---
+++ api/Api/AirportApi/AirportDbContex.cs
@@ -0,0 +1,23 @@
+﻿using AirportApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AirportApi
+{
+    public class AirportDbContext : DbContext
+    {
+        public AirportDbContext(DbContextOptions<AirportDbContext> options) : base (options)
+        {
+        }
+
+        public DbSet<Airport> Airports { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Airport>().ToTable("Airports");
+        }
+    }
+}
---
+++ api/Api/AirportApi/Controllers/AirportsController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using AirportApi;
+using AirportApi.Models;
+
+namespace AirportApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AirportsController : ControllerBase
+    {
+        private readonly AirportDbContext _context;
+
+        public AirportsController(AirportDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Airports
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Airport>>> GetAirports()
+        {
+            return await _context.Airports.ToListAsync();
+        }
+
+        // GET: api/Airports/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Airport>> GetAirport(long id)
+        {
+            var airport = await _context.Airports.FindAsync(id);
+
+            if (airport == null)
+            {
+                return NotFound();
+            }
+
+            return airport;
+        }
+
+        // PUT: api/Airports/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAirport(long id, Airport airport)
+        {
+            if (id != airport.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(airport).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AirportExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Airports
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Airport>> PostAirport(Airport airport)
+        {
+            _context.Airports.Add(airport);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetAirport", new { id = airport.Id }, airport);
+        }
+
+        // DELETE: api/Airports/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAirport(long id)
+        {
+            var airport = await _context.Airports.FindAsync(id);
+            if (airport == null)
+            {
+                return NotFound();
+            }
+
+            _context.Airports.Remove(airport);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool AirportExists(long id)
+        {
+            return _context.Airports.Any(e => e.Id == id);
+        }
+    }
+}
