e59854934490642df7b57cae70e3a8590c1c676e
kriyandi <kriyandimitrov@gmail.com>
2021-08-24 00:39:27 +0300

    Modify DatabaseContext
    Add FlightSection and moved code into separate configuration classes

--- api/WebAbsApi/WebAbsApi/Data/DatabaseContext.cs
+++ api/WebAbsApi/WebAbsApi/Data/DatabaseContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using WebAbsApi.Configurations.Entities;
 
 namespace WebAbsApi.Data
 {
@@ -9,104 +10,17 @@ namespace WebAbsApi.Data
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
-            builder.Entity<Airport>()
-                .HasIndex(e => e.Name)
-                .IsUnique(true);
+            base.OnModelCreating(builder);
 
-            builder.Entity<Airport>().HasData(
-                new Airport
-                {
-                    Id = 1,
-                    Name = "AAY"
-                },
-                new Airport
-                {
-                    Id = 2,
-                    Name = "ABA"
-                },
-                new Airport
-                {
-                    Id = 3,
-                    Name = "MAC"
-                },
-                new Airport
-                {
-                    Id = 4,
-                    Name = "FSD"
-                },
-                new Airport
-                {
-                    Id = 5,
-                    Name = "ZAG"
-                });
-
-            builder.Entity<Airline>(e =>
-            {
-                e.HasKey(e => e.Id);
-                e.HasIndex(e => e.Name).IsUnique(true);
-            });
-
-            builder.Entity<Airline>().HasData(
-                new Airline
-                {
-                    Id = 1,
-                    Name = "DELTA"
-                },
-                new Airline
-                {
-                    Id = 2,
-                    Name = "SSSKY"
-                },
-                new Airline
-                {
-                    Id = 3,
-                    Name = "FLYSS"
-                },
-                new Airline
-                {
-                    Id = 4,
-                    Name = "ARB"
-                },
-                new Airline
-                {
-                    Id = 5,
-                    Name = "WIZZ"
-                });
-
-            builder.Entity<Flight>().HasData(
-                new Flight
-                {
-                    Id = 1,
-                    FlightNumber = "111111111",
-                    Date = new System.DateTime(2021, 5, 13),
-                    AirlineId = 2,
-                    OriginId = 1,
-                    DestinationId = 5
-                },
-                new Flight
-                {
-                    Id = 2,
-                    FlightNumber = "211111111",
-                    Date = new System.DateTime(2021, 5, 20),
-                    AirlineId = 2,
-                    OriginId = 5,
-                    DestinationId = 1
-                }
-                );
-
-            builder.Entity<Flight>()
-                .HasOne(e => e.OriginAirport)
-                .WithMany(e => e.OriginToFlights)
-                .OnDelete(DeleteBehavior.ClientNoAction);
-
-            builder.Entity<Flight>()
-                .HasOne(e => e.DestinationAirport)
-                .WithMany(e => e.DestinationToFlights)
-                .OnDelete(DeleteBehavior.ClientNoAction);
+            builder.ApplyConfiguration(new AirportConfiguration());
+            builder.ApplyConfiguration(new AirlineConfiguration());
+            builder.ApplyConfiguration(new FlightConfiguration());
+            builder.ApplyConfiguration(new FlightSectionConfiguration());
         }
 
         public DbSet<Airport> Airports { get; set; }
         public DbSet<Airline> Airlines { get; set; }
         public DbSet<Flight> Flights { get; set; }
+        public DbSet<FlightSection> FlightSections { get; set; }
     }
 }
