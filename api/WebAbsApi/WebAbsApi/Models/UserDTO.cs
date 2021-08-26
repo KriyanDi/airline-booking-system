9cfb40473a0feae0ebf38fd3e512608c757608f2
kriyandi <kriyandimitrov@gmail.com>
2021-08-26 09:20:48 +0300

    Add Roles - Admin & User

---
+++ api/WebAbsApi/WebAbsApi/Models/UserDTO.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAbsApi.Models
+{
+    public class LoginUserDTO
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(15, ErrorMessage = "Your Password is limited to", MinimumLength = 8)]
+        public string Password { get; set; }
+    }
+
+    public class UserDTO : LoginUserDTO
+    {
+        public ICollection<string> Roles { get; set; }
+    }
+}
