2fe404707bc99512ffa83e292a49615c8c2d876f
kriyandi <kriyandimitrov@gmail.com>
2021-08-26 09:24:15 +0300

    Add Services for Authentication

---
+++ api/WebAbsApi/WebAbsApi/Services/IAuthManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAbsApi.Models;
+
+namespace WebAbsApi.Services
+{
+    public interface IAuthManager
+    {
+        Task<bool> ValidateUser(LoginUserDTO userDTO);
+        Task<string> CreateToken();
+    }
+}
