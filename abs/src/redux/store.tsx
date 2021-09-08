e75c15490f9eba1f8834583bd2fb5f5e942a97ae
kriyandi <kriyandimitrov@gmail.com>
2021-09-08 13:01:39 +0300

    Modify store

--- abs/src/redux/store.tsx
+++ abs/src/redux/store.tsx
@@ -1,11 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
+import airportReducer from "./slices/airport/airportSlice";
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    airportReducer,
+  },
 });
 
-// Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
 
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
+
+export default store;
