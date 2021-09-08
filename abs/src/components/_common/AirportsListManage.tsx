844c2290ecc6fc7b76acc7d889fae83dda8db73d
kriyandi <kriyandimitrov@gmail.com>
2021-09-08 12:57:36 +0300

    Add AirportListManage

---
+++ abs/src/components/_common/AirportsListManage.tsx
@@ -0,0 +1,28 @@
+import React, { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../../redux/hooks";
+import { IAirport } from "../../redux/slices/airport/airportInterfaces";
+import { deleteAirport, fetchAirports, selectAirports } from "../../redux/slices/airport/airportSlice";
+import ObjectListControl from "./ObjectListControl";
+
+const AirportsListManage = (props: any) => {
+  const dispatch = useAppDispatch();
+  const selector = useAppSelector;
+
+  useEffect(() => {
+    dispatch(fetchAirports());
+  }, [dispatch]);
+
+  let list = selector(selectAirports);
+
+  const onDelete = (element: { id: string }): void => {
+    dispatch(deleteAirport({ id: element.id }));
+  };
+
+  return (
+    <div>
+      <ObjectListControl list={list} onDelete={onDelete} />
+    </div>
+  );
+};
+
+export default AirportsListManage;
