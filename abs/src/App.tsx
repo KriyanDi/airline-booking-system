836d7fa3a8ae715d656b9a6978da91ed0a5454ad
kriyandi <kriyandimitrov@gmail.com>
2021-09-08 13:01:51 +0300

    Modify App component

--- abs/src/App.tsx
+++ abs/src/App.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./components/Home/Home";
+import Navigation from "./components/Navigation/Navigation";
+import LoginPage from "./components/User/Login/LoginPage";
+import RegisterPage from "./components/User/Register/RegisterPage";
 
-function App() {
-  return <div>App</div>;
-}
+const App = () => {
+  const [activeItem, setActiveItem] = useState("/");
+
+  let hasLoggedUser = false;
+
+  return (
+    <Router>
+      <div>
+        {!hasLoggedUser ? <Navigation activeItem={activeItem} setActiveItem={setActiveItem} /> : null}
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/login" exact component={LoginPage} />
+          <Route path="/register" exact component={RegisterPage} />
+        </Switch>
+      </div>
+    </Router>
+  );
+};
 
 export default App;
