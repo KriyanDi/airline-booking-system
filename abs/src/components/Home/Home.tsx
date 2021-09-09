82ab4a0c22c8a544d17135eed5760025ecb3dab5
kriyandi <kriyandimitrov@gmail.com>
2021-09-09 13:32:45 +0300

    Modify Home component

--- abs/src/components/Home/Home.tsx
+++ abs/src/components/Home/Home.tsx
@@ -3,16 +3,22 @@ import { Link } from "react-router-dom";
 import { Button, Segment } from "semantic-ui-react";
 
 const Home = (props: any) => {
+  const { setActiveItem } = props;
+
   return (
     <div>
-      <Segment>
+      <Segment centered>
         <h4>Welcome to the Airline Booking System - please proceed to the next step:</h4>
         <Link to="/login">
-          <Button primary>Login</Button>
+          <Button primary onClick={() => setActiveItem("login")}>
+            Login
+          </Button>
         </Link>
 
         <Link to="/register">
-          <Button secondary>Register</Button>
+          <Button secondary onClick={() => setActiveItem("register")}>
+            Register
+          </Button>
         </Link>
       </Segment>
     </div>
