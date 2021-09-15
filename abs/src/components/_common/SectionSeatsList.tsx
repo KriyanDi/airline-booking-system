707106d5b16ce9e837e50677e44c164c0f1edcf3
kriyandi <kriyandimitrov@gmail.com>
2021-09-15 14:35:28 +0300

    Add SectionSeatsList component

---
+++ abs/src/components/_common/SectionSeatsList.tsx
@@ -0,0 +1,66 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Dropdown, Form, Header, Segment } from "semantic-ui-react";
+import { useAppDispatch } from "../../redux/hooks";
+
+const SectionSeatsList = (props: any) => {
+  const dispatch = useAppDispatch();
+
+  const [seats, setSeats] = useState([{ id: null, row: null, column: null }]);
+  const { sectionId, setSeatId, book, setSelectedSeat } = props;
+
+  const getSectionById = async (obj: { id: number }) => {
+    const response = await axios.get(`https://localhost:44318/api/FlightSection/${obj.id}`);
+    return response.data;
+  };
+
+  useEffect(() => {
+    getSectionById({ id: sectionId }).then((res) => setSeats(res.seats?.filter((s: any) => s.isBooked === false)));
+
+    seatsOptions =
+      seats !== undefined
+        ? seats.map((seat) => ({
+            key: seat.id,
+            text: `${seat.row} --- ${seat.column}`,
+            value: seat.id,
+          }))
+        : null;
+  }, [dispatch, sectionId, book]);
+
+  let seatsOptions =
+    seats !== undefined
+      ? seats.map((seat) => ({
+          key: seat.id,
+          text: `${seat.row} --- ${seat.column}`,
+          value: seat.id,
+        }))
+      : null;
+
+  return (
+    <Segment>
+      <Header as="h3" dividing>
+        Select Seat
+      </Header>
+
+      <Form>
+        <Form.Group widths="equal">
+          <Form.Field
+            control={Dropdown}
+            label="Available Seats"
+            options={seatsOptions}
+            selection
+            onChange={(e: any, data: any) => {
+              console.log(data.value);
+              setSeatId(data.value);
+              if (data.value !== "") {
+                setSelectedSeat(true);
+              }
+            }}
+          />
+        </Form.Group>
+      </Form>
+    </Segment>
+  );
+};
+
+export default SectionSeatsList;
