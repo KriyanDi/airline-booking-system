9f6a8183a296fb0f25acb1f9bbd52f332a39aee5
Kristiyan Dimitrov <kriyandimitrov@gmail.com>
2021-10-07 17:44:34 +0300

    Instruction - add table for flight

--- sql-scripts/ABSDB-script.sql
+++ sql-scripts/ABSDB-script.sql
@@ -17,3 +17,18 @@ create table AIRLINE
  constraint UQ_AIRLINE_NAME unique (NAME),
  constraint CHK_AIRLINE_NAME_VALIDATION check (NAME collate SQL_Latin1_General_CP1_CS_AS = upper(NAME) COLLATE SQL_Latin1_General_CP1_CS_AS and len(NAME) >= 1)
 );
+
+create table FLIGHT
+(FLIGHT_ID sql_variant not null,
+ FLIGHT_NUMBER nchar(10) not null,
+ AIRLINE_ID sql_variant not null,
+ ORIG_AIRPORT_ID sql_variant not null,
+ DEST_AIRPORT_ID sql_variant not null,
+ TAKE_OFF datetime not null
+
+ constraint PK_FLIGHT primary key (FLIGHT_ID),
+ constraint UQ_FLIGHT_NUMBER unique (FLIGHT_NUMBER),
+ constraint FK_AIRLINE_FLIGHT foreign key (AIRLINE_ID) references AIRLINE(AIRLINE_ID),
+ constraint FK_ORIGIN_AIRPORT_FLIGHT foreign key (ORIG_AIRPORT_ID) references AIRPORT(AIRPORT_ID) on update no action on delete no action,
+ constraint FK_DESTINATION_AIRPORT_FLIGHT foreign key (DEST_AIRPORT_ID) references AIRPORT(AIRPORT_ID) on update no action on delete no action
+ );
