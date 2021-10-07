ff9cae9dfc4caafb9c45badc92193a1a55009b27
Kristiyan Dimitrov <kriyandimitrov@gmail.com>
2021-10-07 16:19:51 +0300

    Instruction - add table for airline

--- sql-scripts/ABSDB-script.sql
+++ sql-scripts/ABSDB-script.sql
@@ -8,3 +8,12 @@ create table AIRPORT
  constraint UQ_AIRPORT_NAME unique (NAME),
  constraint CHK_AIRPORT_NAME_VALIDATION check (NAME collate SQL_Latin1_General_CP1_CS_AS = upper(NAME) COLLATE SQL_Latin1_General_CP1_CS_AS)
 );
+
+create table AIRLINE
+(AIRLINE_ID sql_variant not null,
+ NAME nvarchar(5) not null,
+
+ constraint PK_AIRLINE primary key (AIRLINE_ID),
+ constraint UQ_AIRLINE_NAME unique (NAME),
+ constraint CHK_AIRLINE_NAME_VALIDATION check (NAME collate SQL_Latin1_General_CP1_CS_AS = upper(NAME) COLLATE SQL_Latin1_General_CP1_CS_AS and len(NAME) >= 1)
+);
