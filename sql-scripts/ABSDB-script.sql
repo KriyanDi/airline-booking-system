c848e59b9b87b739657c4f6be6c744e3ba7ac6fa
Kristiyan Dimitrov <kriyandimitrov@gmail.com>
2021-10-07 15:57:12 +0300

    Instruction - add table for airport

--- sql-scripts/ABSDB-script.sql
+++ sql-scripts/ABSDB-script.sql
@@ -1,3 +1,10 @@
 use ABSDB;
 
+create table AIRPORT
+(AIRPORT_ID sql_variant not null,
+ NAME nchar(3) not null,
 
+ constraint PK_AIRPORT primary key (AIRPORT_ID),
+ constraint UQ_AIRPORT_NAME unique (NAME),
+ constraint CHK_AIRPORT_NAME_VALIDATION check (NAME collate SQL_Latin1_General_CP1_CS_AS = upper(NAME) COLLATE SQL_Latin1_General_CP1_CS_AS)
+);
